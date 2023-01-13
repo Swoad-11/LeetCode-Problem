2ba63183e0c945bc05449ad94ecef07fcf8c3de6
Swoad-11 <toufiq11swoad@gmail.com>
2023-01-13 21:57:29 +0600

    6th problem

---
+++ Final_Value_of_Variable_After_Performing_Operations.js
@@ -0,0 +1,10 @@
+var finalValueAfterOperations = function (operations) {
+    let X = 0
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i][1] == '+')
+            X++;
+        else
+            X--
+    }
+    return X;
+};
