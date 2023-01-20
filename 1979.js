1c39f0fc487feb6033088a421fee148a4672c248
Swoad-11 <toufiq11swoad@gmail.com>
2023-01-20 23:46:44 +0600

    27th problem

---
+++ 1979.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var gcd = function (a, b) {
+
+    if (a == 0 && b == 0)
+        return 0;
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a; e
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
+}
+
+var findGCD = function (nums) {
+    let sorted = nums.sort((a, b) => a - b);
+    let a = sorted[0];
+    let b = sorted[nums.length - 1]
+    return gcd(a, b);
+};
